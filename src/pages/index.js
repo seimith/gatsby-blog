538a657f9e9dfbc2566b39f55a4914cd83d1e6ef
= <seimith@gmail.com>
2019-03-12 13:53:12 -0700

    build(posts): Add list if posts to a gatsby blog with a graphql page query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,43 @@
 import React from "react";
+import { graphql } from 'gatsby';
 import Header from '../components/Header'
 
-const Layout = () => {
-  return (<Header/>)
+const Layout = ({data}) => {
+  const { edges } = data.allMarkdownRemark
+  return (
+    <div>
+      <Header/>
+      {edges.map(edge => {
+        const { frontmatter } = edge.node;
+        return (
+          <div key={frontmatter.path}>
+            {frontmatter.title}
+          </div>
+        )
+      })}
+    </div>
+  )
 };
+
+export const query = graphql`
+  query HompageQuery {
+    allMarkdownRemark(
+      sort: {
+        order: DESC,
+        fields: [frontmatter___date]
+      }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Layout;
