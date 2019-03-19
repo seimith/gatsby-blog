8f2dc7456f527815c64195170e561c453c0c7128
= <seimith@gmail.com>
2019-03-18 17:02:15 -0700

    build(blog post): Build a blog post template with GraphQL and Gatsby

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 const Template = (props) => {
+  const title = props.data.markdownRemark.frontmatter.title;
+  const html = props.data.markdownRemark.html
   return (
     <div>
-      Blog post here.
+      <h1>{title}</h1>
+      <div className="blogpost" dangerouslySetInnerHTML={{__html: html}} />
     </div>
   )
 };
-
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: {eq: $pathSlug} }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
 export default Template;
