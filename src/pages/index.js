2fc7da457830c2283f40207c7e8b9bfd03ffdf5c
= <seimith@gmail.com>
2019-06-14 11:21:48 -0700

    feat(Splash page): Add splash page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,24 +6,15 @@ const Layout = ({data}) => {
   const { edges } = data.allMarkdownRemark
   return (
     <div>
-      <h2>Index</h2>
-      <Header/>
-      {edges.map(edge => {
-        const { frontmatter } = edge.node;
-        return (
-          <div key={frontmatter.path}>
-            <Link to={frontmatter.path}>
-              {frontmatter.title}
-            </Link>
-            <p>{frontmatter.excerpt}</p>
-          </div>
-        )
-      })}
-
-      <div>
-        <Link to='/tags'>Browse by Tag</Link>
-        <br/>
-        <Link to='/categories'>Browse by categories</Link>
+      <div class="media-object">
+        <img src="//raw.githubusercontent.com/seimith/seimith.github.io/master/_assets/me.jpg" alt="Me doing things." class="avatar"/>
+        <h1>Kaixo!</h1>
+        <p>I <Link to='/categories/code' title='Technical posts.'>code</Link> some <Link to='/tags' title='All post categories.'>things</Link>, I <Link to='/categories/design' title='Design related posts.'>design</Link> sometimes, and I <a href="#" class="weekend">do random stuff</a> on the weekends.</p>
+        <p>On <a href="https://twitter.com/themcsuth/" target="_blank" title="Twitter.">social</a> media I'm pretty dead.</p>
+        <p>- "<a href="https://www.linkedin.com/in/seimith/" target="_blank" title="Reach out to me on LinkedIn for work-related inquiries.">Smith</a>"</p>
+      </div>
+      <div class="special-treat">
+        🍭 Here's a <a href="#" class="treat" target="_blank">video</a>.
       </div>
     </div>
   )
